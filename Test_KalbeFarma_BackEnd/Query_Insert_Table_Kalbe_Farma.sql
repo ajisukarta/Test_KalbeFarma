7996c689d88e0991cd2007fb949aa81bc3f54305
Aji Sukarta <50736517+ajisukarta@users.noreply.github.com>
2021-08-06 17:41:33 +0700

    push query sql

---
+++ Test_KalbeFarma_BackEnd/Query_Insert_Table_Kalbe_Farma.sql
@@ -0,0 +1,20 @@
+INSERT INTO Currency VALUES('United States Dollar - USD', 'USD')
+INSERT INTO Currency VALUES('European Union Euro - EUR','EUR')
+INSERT INTO Currency VALUES('Indonesian Rupiah - IDR', 'IDR')
+INSERT INTO Currency VALUES('Malaysian Ringgit','MYR')
+INSERT INTO Currency VALUES('Japanese Yen - JPY', 'JPY')
+INSERT INTO Currency VALUES('Chinese Yuan - CNY','CNY')
+INSERT INTO Currency VALUES('Korean Won - KRW', 'KRW')
+INSERT INTO Currency VALUES('Great Britain Poundsterling - GBP','GBP')
+
+INSERT INTO Recipient VALUES('Bambang Pamungkas','Jl. RS Fatmawati Jakarta Selatan', 'Jakarta','Indonesia','https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Lambang_Persija_Jakarta.svg/1200px-Lambang_Persija_Jakarta.svg.png' )
+INSERT INTO Recipient VALUES('Jordan Henderson','14 Anfield Road St', 'Liverpool','United Kingdom','https://upload.wikimedia.org/wikipedia/id/thumb/0/0c/Liverpool_FC.svg/1200px-Liverpool_FC.svg.png')
+INSERT INTO Recipient VALUES('LeBron James','23 Downing Street Los Angeles', 'California','United States','https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Los_Angeles_Lakers_logo.svg/1200px-Los_Angeles_Lakers_logo.svg.png')
+
+
+INSERT INTO PurchaseOrder VALUES('FL-001',10,'Trent-Alexander Arnold')
+INSERT INTO PurchaseOrder VALUES('FL-002',15,'Stephen Curry')
+INSERT INTO PurchaseOrder VALUES('FL-003',25,'Takumi Minamino')
+
+INSERT INTO [Language] VALUES('English (US)')
+INSERT INTO [Language] VALUES('Indonesian (ID)')
